5da8dd7549039288543a9198923e8f59978990d9
Eduardo o Terrível <dudu_spiderman@hotmail.com>
2021-03-17 15:36:44 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user)
+    return response.status(404).json({ error: "User not found!" });
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ((user.pro == false && user.todos.length < 10) || (user.pro == true))
+    next();
+  else
+    return response.status(403).json({ error: "Not possible to create Todo" });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user)
+    return response.status(404).json({ error: "User not found" });
+
+  const validId = validate(id);
+
+  if (!validId)
+    return response.status(400).json({ error: "This id is not uuid" });
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  
+  if (user && validId && todo) {
+    request.user = user;
+    request.todo = todo;
+    next();
+  }else {
+    return response.status(404).json({ error: "Todo not found" });
+  }
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
